1e4288ccb3362bb67a6e893f036d7205a380b8d5
Christos Plm <xrpala@hotmail.com>
2025-12-09 18:31:53 +0200

    Added task_manager.py

---
+++ task_manager.py
@@ -0,0 +1,13 @@
+# task_manager.py
+
+# Print the menu
+print("=== Task Manager ===")
+print("1. Add Task")
+print("2. View Tasks")
+print("3. Exit")
+
+# Get user's choice - THIS is where you add the basic input
+choice = input("Enter your choice (1-3): ")
+
+# Print what they chose (just to test it works)
+print(f"You chose: {choice}")
